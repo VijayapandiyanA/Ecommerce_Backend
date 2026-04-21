6a919717981e7f7921371e81c287921bd8227d05
VijayapandiyanA <vijayapandiyan@hubino.com>
2026-04-21 15:05:30 +0530

    fix db variables

--- src/app.ts
+++ src/app.ts
@@ -46,8 +46,8 @@ app.get('/',(req:Request,res:Response)=>{
 const PORT =process.env.PORT ||5000
 const ConnectDB =async()=>{
     try{
-        await sequelize.authenticate();
-        await sequelize.sync({ alter: true });
+       await sequelize.sync({ alter: true });
+       await sequelize.sync();
         console.log("Database connected successfully")
         app.listen(PORT,()=>{
             console.log(`Server is running on Port ${PORT}`)
