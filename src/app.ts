a6185a8d73a66555161b30816fd60e84b905e0f8
VijayapandiyanA <vijayapandiyan@hubino.com>
2026-04-21 12:49:47 +0530

    fix cors for vercel frontend

--- src/app.ts
+++ src/app.ts
@@ -18,11 +18,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ecommerce-frontend-aas3.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    origin: true,
     credentials: true
   })
 );
