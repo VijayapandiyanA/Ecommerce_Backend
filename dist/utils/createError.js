6a261f94f07eacc993644948ef51486246abfff9
VijayapandiyanA <vijayapandiyan@hubino.com>
2026-04-21 09:25:27 +0530

    deploy

--- dist/utils/createError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createError = (message, statusCode) => {
-    const err = new Error(message);
-    err.statusCode = statusCode;
-    err.success = false;
-    return err;
-};
-exports.default = createError;
