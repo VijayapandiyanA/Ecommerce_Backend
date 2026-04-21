a0ff74ef8aabd3d95544a8293576dee873b9df9e
VijayapandiyanA <vijayapandiyan@hubino.com>
2026-04-21 14:16:41 +0530

    fix cors for vercel frontend

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,18 @@
-import {Sequelize} from 'sequelize'
-import dotenv from "dotenv"
-dotenv.config()
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const sequelize = new Sequelize(
-    process.env.DATABASE_URL as string,
-{
-    host:process.env.DB_HOST as string,
-    dialect:"mysql",
-    logging:false
+  process.env.DB_DATABASE as string,
+  process.env.DB_USERNAME as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: process.env.DB_HOST as string,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
-}
-)
+
+
