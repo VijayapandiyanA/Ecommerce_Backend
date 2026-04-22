363653cc595345a35a1166a73fb3bed8f546674a
VijayapandiyanA <vijayapandiyan@hubino.com>
2026-04-22 16:30:17 +0530

    changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,10 +4,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "mysql",
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_DATABASE as string,
+      process.env.DB_USERNAME as string,
+      process.env.DB_PASSWORD as string,
+      {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        dialect: "mysql",
+        logging: false,
+      }
+    );
 
 export default sequelize;
 
